c2df5c4e4b47f003167f768f0983113cfab04b72
Luke Buitendijk <lukebuitendijk@gmail.com>
2014-06-05 17:41:50 +0200

    3 klasses gemaakt. Logger, Webserver en SettingsReader. SettingsReader is voor nu af. Het leest een bestandje (vanaf de exe het pad "data/Settings.Dat") uit en laad die instellingen in. De instelling zijn op te halen door de klasse aan te spreken met de juiste property. Logger is leeg. WebServer bevat een demo webclient, deze is fout en kan niet gebruikt worden. (Geen HttpListener, maar TcpListener) Program start nu 2 servers op, een normale en een admin. (localhost:8080 voor normale en 8081 voor admin)

--- Eindopdracht/Program.cs
+++ Eindopdracht/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,30 @@ namespace Eindopdracht
 {
     public class Program
     {
-        static void Main(string[] args)
+        private static SettingsReader settings;
+        public static void Main(string[] args)
         {
+            settings = new SettingsReader();
+
+            Webserver ws = new Webserver(SendResponse, "http://localhost:" + settings.Port + "/");
+            Webserver aw = new Webserver(SendAdminResponse, "http://localhost:" + settings.AdminPort + "/");
+            ws.Run();
+            aw.Run();
+
+            Console.WriteLine("A simple webserver. Press a key to quit.");
+            Console.ReadKey();
+            ws.Stop();
+            aw.Stop();
+        }
+
+        public static string SendResponse(HttpListenerRequest request)
+        {
+            return string.Format("<html><body>My web page.<br>{0}</body></html>", DateTime.Now);
+        }
+
+        public static string SendAdminResponse(HttpListenerRequest request)
+        {
+            return string.Format("<html><body>My admin web page.<br>{0}</body></html>", DateTime.Now);
         }
     }
 }
---
+++ Eindopdracht/SettingsReader.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eindopdracht
+{
+    public class SettingsReader
+    {
+        public int Port { get; private set; }
+        public int AdminPort { get; private set; }
+        public String Root { get; private set; }
+        public String DefaultPage { get; private set; }
+        public Boolean DirectoryBrowsing { get; private set; }
+
+        /// <summary>
+        /// Provides default settings and provides them.
+        /// </summary>
+        public SettingsReader()
+        {
+            ReadSettingsFile();
+        }
+
+        public void Reload()
+        {
+            ReadSettingsFile();
+        }
+
+        private void ReadSettingsFile()
+        {
+            StreamReader sr;
+            String sLine = "";
+
+            try
+            {
+                sr = new StreamReader("data\\Settings.Dat");
+
+                while ((sLine = sr.ReadLine()) != null)
+                {
+                    sLine.Trim();
+
+                    if (sLine.Length > 0)
+                    {
+                        int iPosType = sLine.IndexOf("=");
+
+                        String sType = sLine.Substring(0, iPosType);
+                        String sValue = sLine.Substring(iPosType + 1);
+
+                        switch (sType)
+                        {
+                            case "Port":
+                                Port = int.Parse(sValue);
+                                break;
+                            case "AdminPort":
+                                AdminPort = int.Parse(sValue);
+                                break;
+                            case "Root":
+                                Root = sValue;
+                                break;
+                            case "DefaultPage":
+                                DefaultPage = sValue;
+                                break;
+                            case "DirectoryBrowsing":
+                                DirectoryBrowsing = Boolean.Parse(sValue);
+                                break;
+                            default:
+                                Console.WriteLine("Unknown Setting found: {0}", sType);
+                                break;
+                        }
+                    }
+                }
+
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An Exception Occurred : " + e.ToString());
+            }
+        }
+    }
+}
