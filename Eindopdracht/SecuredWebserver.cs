6c10aefeb9543bc36e40964594da86f0cfdd9e91
Luke Buitendijk <lukebuitendijk@gmail.com>
2014-06-17 22:10:10 +0200

    SSL uit gecommentarieerd. Gedeeltelijk structuur aangepast. Ook heb ik de GET werkend gemaakt. (als het goed is) En bij de POST wordt de variabelen uitgelezen.

--- Eindopdracht/SecuredWebserver.cs
+++ Eindopdracht/SecuredWebserver.cs
@@ -14,42 +14,59 @@ namespace Eindopdracht
 {
     public class SecuredWebserver : AbstractWebserver
     {
+        /* Voor SSL
         public static string cerficicate_name = @"TempCert.pfx";
+        private X509Certificate certificate;
+        */
 
         private SettingsReader Settings;
         private TcpListener listener;
-        private X509Certificate certificate;
 
         public SecuredWebserver(SettingsReader Settings)
             : base(Settings)
         {
             this.Settings = Settings;
             listener = new TcpListener(IPAddress.Parse("127.0.0.1"), Settings.AdminPort);
-            certificate = new X509Certificate2(cerficicate_name, "ChrisLuke");
-            // X509Certificate.CreateFromCertFile(cerficicate_name);
+            //certificate = new X509Certificate2(cerficicate_name, "ChrisLuke");
         }
 
         public override void StartListening()
         {
             listener.Start();
-            Console.WriteLine("Secure server online, listening");
+            Console.WriteLine("'Secure' server online, listening");
 
             while (true)
             {
+                /* Voor SSL
                 TcpClient client = listener.AcceptTcpClient();
-                SslStream stream = new SslStream(client.GetStream(), false);
 
                 //stream.ReadTimeout = 5000;
                 //stream.WriteTimeout = 5000;
+                */
 
+                // Debug, SSL te ontwijken
+                Socket sClient = listener.AcceptSocket();
+                Console.WriteLine("Socket Type: " + sClient.SocketType);
+                if (sClient.Connected)
+                    Console.WriteLine("Client Connected\nClient IP: {0}", sClient.RemoteEndPoint);
+
+                /* SSL afhandeling
                 Console.WriteLine("Socket Type: " + client.Client.SocketType);
                 if (client.Connected)
                     Console.WriteLine("Client Connected\nClient IP: {0}", client.Client.RemoteEndPoint);
+                */
 
                 try
                 {
+                    /* SSL Afhandeling
                     stream.AuthenticateAsServer(certificate, false, SslProtocols.Tls, true);
                     String message = readMessage(stream);
+                    Console.WriteLine(message);
+                    */
+
+                    Byte[] byteMessage = new Byte[1024];
+                    int i = sClient.Receive(byteMessage, byteMessage.Length, 0);
+                    String message = Encoding.ASCII.GetString(byteMessage);
                     Console.WriteLine(message);
 
                     int iFirstPos = message.IndexOf(' ');
@@ -57,101 +74,95 @@ namespace Eindopdracht
 
                     if (iFirstPos > 0 && iLastPos > 0)
                     {
-                        String rType = message.Substring(0, iFirstPos);
-                        String rURL = message.Substring(iFirstPos + 1, iLastPos - (iFirstPos + 1)).Replace("\\", "/");
-                        String rHTML = message.Substring(iLastPos + 1, 8);
+                        String type = message.Substring(0, iFirstPos);
+                        String url = message.Substring(iFirstPos + 1, iLastPos - (iFirstPos + 1)).Replace("\\", "/");
+                        String html = message.Substring(iLastPos + 1, 8);
 
-                        if (!rType.Equals("GET") && !rType.Equals("POST"))
+                        if (!type.Equals("GET") && !type.Equals("POST"))
                         {
-                            Console.WriteLine("Unsupported request type encountered: {0}", rType);
+                            Console.WriteLine("Unsupported request type encountered: {0}", type);
                             // Send Error 400
-                            client.Close();
+                            //client.Close(); Nodig voor SSL
                             continue;
                         }
 
-                        Console.WriteLine("Request type: {0}\nRequest URL: {1}\nRequest HTML: {2}", rType, rURL, rHTML);
+                        Console.WriteLine("Request type: {0}\nRequest URL:  {1}\nRequest HTML: {2}", type, url, html);
 
-                        String sStatus = "";
-                        // Will be a path to a 40x page if something went wrong or default file or the requested file
-
-                        Byte[] bByteFile = null;
-                        String path = HandleURLRequest(rURL, out sStatus);
-                       
-                        if (!String.IsNullOrWhiteSpace(path))
+                        if (type.Equals("GET"))
                         {
-                            FileStream fStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read);
-                            BinaryReader bReader = new BinaryReader(fStream);
+                            String status = "";
+                            // Will be a path to a 40x page or the requested file
 
-                            byte[] bytes = new byte[fStream.Length];
-                            int read;
-                            while ((read = bReader.Read(bytes, 0, bytes.Length)) != 0) { }
+                            Byte[] byteFile = null;
+                            String path = HandleGETRequest(url, out status);
 
-                            bByteFile = bytes;
-
-                            bReader.Close();
-                            fStream.Close();
-                        }
-                        else // This only occurs when DirectoryBrowsing is on and the Directory exists
-                        {
                             try
                             {
-                                rURL = rURL.EndsWith("/") ? rURL : rURL + "/";
-                                String sHTML = "";
-                                String templateHTML = "";
-                                String templateList = "";
-                                String templateListItem = "";
+                                String htmlPage = "";
 
-                                using (StreamReader sr = new StreamReader("Template\\DirectoryBrowsing.html"))
-                                    templateHTML = sr.ReadToEnd();
+                                using (StreamReader sr = new StreamReader(path))
+                                    htmlPage = sr.ReadToEnd();
 
-                                using (StreamReader sr = new StreamReader("Template\\DirBrowseList.html"))
-                                    templateListItem = sr.ReadToEnd();
+                                htmlPage = htmlPage.Replace("{LoginError}", "");
 
-                                foreach (String sItem in GetAllFilesFromDirectory(rURL))
-                                    templateList += templateListItem.Replace("{Item}", sItem);
-
-                                sHTML = templateHTML.Replace("{List}", templateList).Replace("{URL}", rURL);
-                                bByteFile = Encoding.ASCII.GetBytes(sHTML);
+                                byteFile = Encoding.ASCII.GetBytes(htmlPage);
                             }
                             catch (Exception e)
                             {
-                                Console.WriteLine("The template could not be read. Message:");
+                                Console.WriteLine("File could not be read. Message:");
                                 Console.WriteLine(e.Message);
                             }
+
+                            //Socket socket = client.Client; Nodig voor SSL
+                            SendHeader(html, status, "text/html", byteFile.Length, ref sClient);
+                            SendData(byteFile, ref sClient);
+                        }
+                        else // PUT
+                        {
+                            String postParams = message.Substring(message.LastIndexOf("\r\n"));
+                            Console.WriteLine("Post parameters: {0}", postParams);
                         }
 
-                        String sMime = GetMime(rURL.Substring(rURL.Contains('.') ? rURL.LastIndexOf('.') : 0));
-
-                        Socket socket = client.Client;
-                        SendHeader(rHTML, sStatus, sMime, bByteFile.Length, ref socket);
-                        SendData(bByteFile, ref socket);
                     }
                 }
 
                 catch (AuthenticationException e)
                 {
                     Console.WriteLine(e.Message);
+                    /*
                     stream.Close();
-                    client.Close();
+                    */
+                    sClient.Close();
+
                     return;
                 }
 
                 catch (IOException e)
                 {
                     Console.WriteLine(e.Message);
+                    /*
                     stream.Close();
-                    client.Close();
+                    */
+                    sClient.Close();
+
                     return;
                 }
                 
                 finally
                 {
+                    /*
                     stream.Close();
-                    client.Close();
+                    */
+
+                    sClient.Close();
                 }
             }
         }
-
+        /// <summary>
+        /// Uses an SslStream to read the message.
+        /// </summary>
+        /// <param name="stream">The SslStream</param>
+        /// <returns></returns>
         private String readMessage(SslStream stream)
         {
             byte[] buffer = new byte[2048];
@@ -174,46 +185,42 @@ namespace Eindopdracht
             return messageData.ToString();
         }
 
-        private String HandleURLRequest(String rURL, out String sStatusCode)
+        private String HandleGETRequest(String url, out String statusCode)
         {
-            sStatusCode = "200";
+            statusCode = "200";
+            String path = "";
 
-            int iPos;
-            String fileName = "";
-            String path = GetLocalPath(rURL);
-
-            // First check if there is something behind the /
-            // Secondly check what after the / stands contains a .
-            // Thirdly check if the length of the filename (that what stands behind the / ) is longer or equal than 3
-            // Lastly check if the file has a name or extention which is atleast 1 character long, by checking if the . is within the appropriate bounds.
-            // If so, it's a file (/dir/f.l or /directory/file.withextenstion)
-            if ((iPos = rURL.LastIndexOf('/') + 1) < rURL.Length && (fileName = rURL.Substring(iPos)).Contains('.') && (fileName.LastIndexOf('.') >= 1 && fileName.LastIndexOf('.') < fileName.Length - 1))
+            switch (url)
             {
-                if (File.Exists(path))
-                    return path; // The file exists, return the path
+                case "/":
+                    path = "index.html";
+                    break;
+                case "/log":
+                    break;
+                case "/users":
+                    break;
+                case "/create":
+                    break;
+                case "/edit":
+                    break;
+                case "/delete":
+                    break;
+                case "/deleteUser":
+                    break;
+                default:
+                    break;
+            }
 
+            if (String.IsNullOrWhiteSpace(path))
+            {
                 // The file does not exist, return a 404 page
-                sStatusCode = "404";
+                statusCode = "404";
                 return "ErrorPages\\404.html";
             }
-            else // If not it's a directory (/dir or /dir/
+            else
             {
-                if (Directory.Exists(path))
-                {
-                    path = path.EndsWith("/") ? path : path + '/';
-                    String file = GetTheDefaultFileName(path.EndsWith("/") ? path : path + '/'); // Makes sure that the last char is a /
-                    if (!String.IsNullOrWhiteSpace(file))
-                        return path + file; // The default file exists, return the path
-
-                    // No defaultpage exists, check if DirectoryBrowsing aanstaat
-                    if (Settings.DirectoryBrowsing)
-                        // Needs special handling. (there's no default file, needs to be dynamic)
-                        return "";
-                }
-
-                // The directory does not exist, return a 404 page
-                sStatusCode = "404";
-                return "ErrorPages\\404.html";
+                path = "SecuredPages\\" + path;
+                return path;
             }
         }
     }
